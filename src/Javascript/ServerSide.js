053e052c3dd6a00e215ffab894d1b6751d6c1eed
leewjae <wonjae.lee@dremio.com>
2022-08-05 01:25:39 -0400

    Added Serverside methods for task1

--- src/Javascript/ServerSide.js
+++ src/Javascript/ServerSide.js
@@ -1,12 +1,70 @@
 /**
+ * word with capital represents class.
+ */
+
+/**
  * This Funtion returns all Post Object from the server
- * @returns post
+ * @returns List<Post>
  */
 const getAllPost = () => {
     return null
 }
 
-
-const getPostsWithFilter = (filter) => {
+/**
+ * This Funtion returns all Post Object from the server by the user
+ * filtered by the designated user's uuid.
+ * @param UUID
+ * @returns List<Post>
+ */
+const getPostsBy = (uuid) => {
     return null
+}
+
+/**
+ * This Funtion returns all Post Object from the server Except the designated user's uuid.
+ * @param UUID
+ * @returns List<Post>
+ */
+ const getPostsExcept = (uuid) => {
+    return null
+}
+
+/**
+ * make post using the Post Object
+ * return true if the operation was successful.
+ * @param Post
+ * @returns boolean
+ */
+const makePost = (post) => {
+    return;
+}
+
+
+/**
+ * make Comment using the Comment Object
+ * return true if the operation was successful.
+ * @param Comment
+ * @returns boolean
+ */
+const makeComment = (Comment) => {
+    return;
+}
+
+/**
+ * get All Comments filtered by the UUID of the post
+ * @param UUID
+ * @returns List<Comments>
+ */
+const getAllCommentOf = (uuid) => {
+
+}
+
+/**
+ * increase like of Post that matches UUID
+ * return true if the operation was successful
+ * @param UUID
+ * @returns boolean
+ */
+const like = (uuid) => {
+
 }
