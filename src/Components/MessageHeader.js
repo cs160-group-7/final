9502bacfc8d68d5ada1399faf171248fe98ce350
Hohyeon Moon <hohyeonmoon@gmail.com>
2022-08-10 02:08:43 +0900

    Bug fix

--- src/Components/MessageHeader.js
+++ src/Components/MessageHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/message.css'
 import homeicon from '../assets/message-home.svg';
+import { useNavigate } from 'react-router-dom';
 
 
 const MessageHeader = () => {
