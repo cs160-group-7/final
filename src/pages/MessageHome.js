060cffdc8ae6ac9d16e57484f3b486b0585a4c5b
jinkim2739 <67286328+jinkim2739@users.noreply.github.com>
2022-08-09 03:29:02 -0700

    message home ui complete

--- src/pages/MessageHome.js
+++ src/pages/MessageHome.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import MessageHeader from '../Component/MessageHeader';
+import mailimage from '../assets/mail-image.svg';
+
+const WelcomeText = () => {
+    var month_arr = ["Jan","Feb","Mar","April", "June","July","Aug","Sep", "Oct", "Nov", "Dec"]
+    const d = new Date();
+    const month = month_arr[d.getMonth() - 1];
+    const date = d.getDate();
+    console.log(month);
+    console.log(date);
+    return(
+        <div className ="welcome-text-wrap">
+          <h3 className = "welcome-text">Good Morning <em>Name</em>,</h3>
+          <h3 className = "welcome-text">Today is <em>{month} {date}</em>.</h3>
+        </div>
+    );
+}
 
 const MessageHome = () => {
     return(
         <div>
             <MessageHeader/>
+            <WelcomeText/>
+            <div>
+                <button className="mail-button"><img src={mailimage}/></button>
+            </div>
+            <button className="makesomeonesday">
+               <span>MAKE SOMEONE'S DAY</span>
+            </button>
         </div>
     )
 }
