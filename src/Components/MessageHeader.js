307864cb457631bc08862527d7d714ff5e059ddd
jinkim2739 <67286328+jinkim2739@users.noreply.github.com>
2022-08-09 11:32:07 -0700

    navigate to home in all pages

--- src/Components/MessageHeader.js
+++ src/Components/MessageHeader.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../styles/message.css'
 import homeicon from '../assets/message-home.svg';
-
+import { useNavigate } from 'react-router-dom';
 
 const MessageHeader = () => {
+    const navigate = useNavigate();
+    function goHome() {
+        navigate("/home")
+    }
     return(
         <div className="header-container">
-            <button className="home-button">
+            <button className="home-button" onClick={goHome}>
                 <img src={homeicon}/>
             </button>
             <span className="header-name">MESSAGE</span>
